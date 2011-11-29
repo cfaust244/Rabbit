4025abd2bd78621128cffe2a68d0c0f712df1014
Cody Faust <cfaust244@gmail.com>
2011-11-28 22:27:25 -0600

    Added multiple functions to read the sources and installed package lists, see the changelog for more details.

--- rabbit.hs
+++ rabbit.hs
@@ -1,5 +1,12 @@
+-- Rabbit OSX Application Manager v1.0
+-- Cody Faust <cfaust244@gmail.com>
+-- Uses split from cabal
+
 module Main where
+
 import System.Environment
+import System.IO
+import Data.List.Split
 
 -- main, just gets the command line args and sends them to the clController
 main :: IO()
@@ -15,6 +22,7 @@ clController (x:xs)
               | x == "update"  = putStrLn "Updating..."
               | x == "remove"  = uninstall xs
               | x == "help"    = help
+              | x == "list-all"= listCarrots $ readCarrots
               | otherwise = putStrLn "Unknown Command"
 
 -- Verifies it can install the given applications, and then calls the necessary 
@@ -30,7 +38,37 @@ uninstall :: [String] -> IO()
 uninstall [] = putStrLn "Please enter what to uninstall"
 uninstall xs  = putStrLn ("Uninstalling " ++ (unwords xs))
 
+-- Just prints out a simple help menu
+-- TODO: Clean up help menu
 help :: IO()
 help = do putStrLn "install application... installs the application or applications supplied"
           putStrLn "update... updates all installed packages"
           putStrLn "remove application...removes the supplied application or applications"
+
+-- Reads the carrots.list file and returns an IO String for whatever needs it
+-- TODO: Make more generic so it works on more than one file? 
+readCarrots :: IO String
+readCarrots =  do contents <- readFile "carrots.list"
+                  return contents
+
+-- Reads the installed.list file and returns an IO String for whatever needs it
+readInstalled :: IO String
+readInstalled =  do contents <- readFile "installed.list"
+                    return contents
+
+-- Lists all carrots availible
+listCarrots :: IO String -> IO()
+listCarrots contents = do result <- contents
+                          putStrLn result
+                                
+-- See if the package is installed
+isInstalled :: IO String -> String -> IO Bool
+isInstalled areInstalled toCheck =  do x <- areInstalled
+                                       if elem toCheck (dropEveryOther (words (unwords (splitOn ":" x)))) then return True else return False
+
+-- Function to remove every other element from a list, used to remove the version when needed
+dropEveryOther [] = []
+dropEveryOther (x:xs)
+                | null xs = x:[]
+                | otherwise = x:(dropEveryOther (tail xs))
+                                      
